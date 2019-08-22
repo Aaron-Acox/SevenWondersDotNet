6b8c76b7fb56f468caaedeafc756ee276e2b458a
Aaron Acox <aaron.acox@icloud.com>
2019-08-21 21:42:44 -0400

    basic get and post for game controller

---
+++ GameApi/Controllers/GameController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+namespace GameApi.Controllers
+{
+    public class GameController : ControllerBase
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpGet("api/game")]
+        public async Task<IActionResult> Get()
+        {
+            var config = new AmazonDynamoDBConfig {ServiceURL = "http://localhost:8000"};
+            var client = new AmazonDynamoDBClient(config);
+            var request = new ScanRequest
+            {
+                TableName = "game"
+            };
+            var response = await client.ScanAsync(request);
+            var games = response.Items.Select(i => new Game{Id = Guid.Parse(i["id"].S)});
+            return Ok(games);
+        }
+
+        [HttpPost("api/game")]
+        public async Task<IActionResult> Post()
+        {
+            var config = new AmazonDynamoDBConfig {ServiceURL = "http://localhost:8000"};
+            var client = new AmazonDynamoDBClient(config);
+            var id = Guid.NewGuid();
+            var request = new PutItemRequest
+            {
+                TableName = "game",
+                Item = new Dictionary<string, AttributeValue>
+                {
+                    {"id", new AttributeValue{S = id.ToString()}}
+                }
+            };
+            var response = await client.PutItemAsync(request);
+            return Created($"api/{id}", new Game {Id = id});
+        }
+    }
+}
---
+++ GameApi/Models/Game.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace GameApi.Models
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+    }
+}
